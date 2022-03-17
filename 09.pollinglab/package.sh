1a96a07a356e6a489078d6a4b06940388e80b71b
Christopher Bohn <bohn@unl.edu>
2022-03-17 14:22:17 -0500

    a little repo cleanup

--- 09.pollinglab/package.sh
+++ 09.pollinglab/package.sh
@@ -4,8 +4,9 @@ cd assignment
 make
 cd ../starter-code
 cp ../assignment/pollinglab.pdf .
+tar cvf pollinglab.tar *
+mv pollinglab.tar ..
 zip pollinglab.zip * */*
 mv pollinglab.zip ..
 mv pollinglab.pdf ..
 cd ..
-
