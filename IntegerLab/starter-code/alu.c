5320f17b517230f8bdff44880ae5c3f5b9790bed
Christopher Bohn <bohn@unl.edu>
2022-12-29 09:43:28 -0600

    Updated IntegerLab to have smaller, bite-sized chunks

--- IntegerLab/starter-code/alu.c
+++ IntegerLab/starter-code/alu.c
@@ -1,61 +1,279 @@
-/*
- * STUDENT: (Type your name here)
- */
-
-/*
- * IntegerLab (c) 2018-21 Christopher A. Bohn
- */
-
-/******************************************************************************
- * In this lab, students are required to implement integer arithmetic for
- * unsigned 16-bit integers and signed (twos complement) 16-bit integers using
- * only bitwise and (&), bitwise or (|), bitwise xor (^), bitwise complement
- * (~), and bit shifts (<< and >>). DO NOT USE built-in addition (+),
- * subtraction (-), multiplication (*), division (/), nor modulo (%).
+/**************************************************************************//**
+ *
+ * @file alu.c
+ *
+ * @author Christopher A. Bohn
+ *
+ * @brief Functions that students must implement for IntegerLab to demonstrate
+ * understanding of boolean logic and bit-limited integer arithmetic.
+ *
  ******************************************************************************/
+
+/*
+ * IntegerLab (c) 2018-22 Christopher A. Bohn
+ *
+ * Assignment and starter code licensed under the Apache License,
+ * Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
+ */
 
 #include "alu.h"
 
-/* Adds the two arguments and stores the sum in the return structure's result
- * field.  If the operation overflowed then the overflow flag is set. */
-addition_subtraction_result add(uint16_t augend, uint16_t addend) {
-    addition_subtraction_result addition;
-    addition.result = augend + addend;                          // THIS IS DISALLOWED
-    addition.overflow = false;                                  // THIS IS WRONG
-    return addition;
+/**
+ * Determines whether a bit vector, when interpreted as a two's complement signed integer, is negative.
+ * @param value the bit vector to be evaluated
+ * @return 1 if the interpreted argument is less than zero; 0 otherwise
+ */
+bool is_negative(uint16_t value) {
+    return false;
 }
 
-/* Subtracts the second argument from the first, stores the difference in the
- * return structure's result field.  If the operation overflowed then the
- * overflow flag is set. */
-addition_subtraction_result subtract(uint16_t menuend, uint16_t subtrahend) {
-    uint16_t augend = menuend;
-    uint16_t addend = (uint16_t)(-subtrahend);                  // THIS IS DISALLOWED
-    return add(augend, addend);
+/**
+ * Determines whether two values are equal. Two values are considered equal if their bit vectors are identical.
+ * @param value1 the first value for the comparison
+ * @param value2 the second value for the comparison
+ * @return 1 if the two arguments are equal; 0 otherwise
+ */
+bool equal(uint16_t value1, uint16_t value2) {
+    return false;
 }
 
-/* Multiplies the two arguments.  The function stores lowest 16 bits of the
- * product in the return structure's product field and the full 32-bit product
- * in the full_product field.  If the product doesn't fit in the 16-bit
- * product field then the overflow flag is set. */
-multiplication_result multiply(uint16_t multiplicand, uint16_t multiplier) {
-    multiplication_result multiplication;
-    multiplication.product = multiplicand * multiplier;         // THIS IS DISALLOWED
-    multiplication.full_product = multiplicand * multiplier;    // THIS IS DISALLOWED
-    multiplication.overflow = false;                            // THIS IS WRONG
-    return multiplication;
+/**
+ * Determines whether two values are not equal. Two values are considered equal if their bit vectors are identical.
+ * @param value1 the first value for the comparison
+ * @param value2 the second value for the comparison
+ * @return 0 if the two arguments are equal; 1 otherwise
+ */
+bool not_equal(uint16_t value1, uint16_t value2) {
+    return false;
 }
 
-/* Divides the first argument by the second.  The function stores the quotient
- * in the return structure's quotient field and the the remainder in the
- * remainder field.  If the divisor is zero then the quotient and remainder
- * fields should be ignored, and the division_by_zero field should be set. */
-division_result divide(uint16_t dividend, uint16_t divisor) {
-    division_result division;
-    if (divisor) {
-        division.quotient = dividend / divisor;                 // THIS IS DISALLOWED
-        division.remainder = dividend % divisor;                // THIS IS DISALLOWED
-    }
-    division.division_by_zero = false;                          // THIS IS WRONG
-    return division;
+/**
+ * Computes the logical inverse of the argument.
+ * @param value the value to be inverted; 0 is considered <code>false</code>, and non-zero values are considered <code>true</code>
+ * @return 1 if the argument is 0; 0 otherwise
+ */
+bool logical_not(uint32_t value) {
+    return false;
+}
+
+/**
+ * Computes the logical conjunction of the arguments.
+ * @param value1 the first operand for the conjunction; 0 is considered <code>false</code>, and non-zero values are considered <code>true</code>
+ * @param value2 the second operand for the conjunction; 0 is considered <code>false</code>, and non-zero values are considered <code>true</code>
+ * @return 1 if both arguments are <code>true</code>; 0 otherwise.
+ */
+bool logical_and(uint32_t value1, uint32_t value2) {
+    return false;
+}
+
+/**
+ * Computes the logical disjunction of the arguments.
+ * @param value1 the first operand for the disjunction; 0 is considered <code>false</code>, and non-zero values are considered <code>true</code>
+ * @param value2 the second operand for the disjunction; 0 is considered <code>false</code>, and non-zero values are considered <code>true</code>
+ * @return 1 if either (or both) argument is <code>true</code>; 0 otherwise.
+ */
+bool logical_or(uint32_t value1, uint32_t value2) {
+    return false;
+}
+
+/**
+ * Performs binary addition for one bit position.
+ * Given input bits a, b, and c_in, computes sum = a + b + c, with c_out (carry_out) as 0 or 1 depending on whether or
+ * not the full sum fits into a single bit.
+ * @param bits the <code>struct</code> with the input bits
+ * @return The <code>struct</code> with the output (and input) bits
+ */
+one_bit_adder_t one_bit_full_addition(one_bit_adder_t bits) {
+//    bits.sum = ...;
+//    bits.c_out = ...;
+    return bits;
+}
+
+/**
+ * Uses 32 one-bit full adders (or, equivalently, uses 1 one-bit full adder 32 times) to add two 32-bit integers.
+ * While a carry-in bit is provided for the least-significant bit, the carry-out bit from the most-significant bit is
+ * not preserved (as it is not needed for any part of this assignment).
+ * @param value1 the first number to be added
+ * @param value2 the second number to be added
+ * @param carry_in The carry-in bit for the least-significant bit's adder
+ * @return the 32-bit sum of the arguments
+ */
+uint32_t ripple_carry_addition(uint32_t value1, uint32_t value2, uint8_t carry_in) {
+    uint8_t carry = carry_in & 0x1;
+    uint32_t sum = 0;
+    return sum;
+}
+
+/**
+ * <p>Adds two 16-bit integers. The arguments are bit vectors that can be interpreted either as unsigned integers or as
+ * signed integers. After computing the sum, this function determines whether overflow occurs when the bit vectors are
+ * interpreted as unsigned integers, and this function also determines whether overflow occurs when the bit vectors are
+ * interpreted as signed integers.</p>
+ *
+ * <p>This function does not alter the ALU's <code>supplemental_result</code> field, and it sets the ALU's
+ * <code>divide_by_zero</code> flag to 0.</p>
+ *
+ * @param augend the number to be added to
+ * @param addend the number to be added to the augend
+ * @return the sum in the ALU's <code>result</code> field, and the <code>unsigned_overflow</code> and <code>signed_overflow</code> flags set appropriately
+ */
+alu_result_t add(uint16_t augend, uint16_t addend) {
+    alu_result_t sum;
+    return sum;
+}
+
+/**
+ * <p>Subtracts two 16-bit integers. The arguments are bit vectors that can be interpreted either as unsigned integers
+ * or as signed integers. After computing the difference, this function determines whether overflow occurs when the bit
+ * vectors are interpreted as unsigned integers, and this function also determines whether overflow occurs when the bit
+ * vectors are interpreted as signed integers.</p>
+ *
+ * <p>This function does not alter the ALU's <code>supplemental_result</code> field, and it sets the ALU's
+ * <code>divide_by_zero</code> flag to 0.</p>
+ *
+ * @param menuend the number to be subtracted from
+ * @param subtrahend the number to be subtracted from the menuend
+ * @return the difference in the ALU's <code>result</code> field, and the <code>unsigned_overflow</code> and <code>signed_overflow</code> flags set appropriately
+ */
+alu_result_t subtract(uint16_t menuend, uint16_t subtrahend) {
+    alu_result_t difference;
+    return difference;
+}
+
+/**
+ * Determines whether the first value is strictly less than the second.
+ * @param value1 the value on the left side of the inequality comparison
+ * @param value2 the value on the right side of the inequality comparison
+ * @return 1 if the first argument is strictly less than the second; 0 otherwise
+ */
+bool less_than(uint16_t value1, uint16_t value2) {
+    return false;
+}
+
+/**
+ * Determines whether the first value is at most the second; that is, whether the first value is less than or equal to
+ * the second.
+ * @param value1 the value on the left side of the inequality comparison
+ * @param value2 the value on the right side of the inequality comparison
+ * @return 1 if the first argument is at most the second; 0 otherwise
+ */
+bool at_most(uint16_t value1, uint16_t value2) {
+    return false;
+}
+
+/**
+ * Determines whether the first value is at least the second; that is, whether the first value is greater than or equal
+ * to the second.
+ * @param value1 the value on the left side of the inequality comparison
+ * @param value2 the value on the right side of the inequality comparison
+ * @return 1 if the first argument is at least the second; 0 otherwise
+ */
+bool at_least(uint16_t value1, uint16_t value2) {
+    return false;
+}
+
+/**
+ * Determines whether the first value is strictly greater than the second.
+ * @param value1 the value on the left side of the inequality comparison
+ * @param value2 the value on the right side of the inequality comparison
+ * @return 1 if the first argument is strictly greater than the second; 0 otherwise
+ */
+bool greater_than(uint16_t value1, uint16_t value2) {
+    return false;
+}
+
+/**
+ * Multiplies two 16-bit integers, producing a 32-bit integer. The second argument <i>must</i> be zero or a power of
+ * two.
+ * @param value the number to be multiplied
+ * @param power_of_two the number that the first is to be multiplied by
+ * @return the full product of the two arguments
+ */
+uint32_t multiply_by_power_of_two(uint16_t value, uint16_t power_of_two) {
+    return 0;
+}
+
+/**
+ * <p>Multiplies two 16-bit integers. The arguments are bit vectors that are interpreted as unsigned integers. The lower
+ * 16 bits of the full product are placed in the ALU's <code>result</code> field, and the upper 16 bits of the full
+ * product are placed in the ALU's <code>supplemental_result</code> field.</p>
+ *
+ * <p>This function sets the ALU's <code>divide_by_zero</code> flag to 0 but it does not alter the ALU's
+ * <code>unsigned_overflow</code> and <code>signed_overflow</code> flags.</p>
+ *
+ * @param multiplicand the number to be multiplied
+ * @param multiplier the number that the first is to be multiplied by
+ * @return the product in the ALU's <code>result</code> and <code>supplemental_result</code> fields
+ */
+alu_result_t unsigned_multiply(uint16_t multiplicand, uint16_t multiplier) {
+    alu_result_t product;
+    return product;
+}
+
+/**
+ * <p>Divides two 16-bit integers. The arguments are bit vectors that are interpreted as unsigned integers.</p>
+ *
+ * <p>The divisor <i>must</i> be zero or a power of two.</p>
+ *
+ * <p>If the divisor is non-zero, the quotient is placed in the ALU's <code>result</code> field, the modulus (or
+ * remainder) is placed in the ALU's <code>supplemental_result</code> field, and the <code>divide_by_zero</code> flag
+ * is set to 0.</p>
+ *
+ * <p>If the divisor is zero, the ALU's <code>divide_by_zero</code> flag is set to 1 and no guarantees are made about
+ * the contents of the <code>result</code> and <code>supplemental_result</code> fields.</p>
+ *
+ * <p>Regardless, the ALU's <code>unsigned_overflow</code> and <code>signed_overflow</code> flags are not altered.</p>
+ *
+ * @param dividend the number to be divided
+ * @param divisor the number that divides the first
+ * @return the ALU's <code>divide_by_zero</code> flag set appropriately, and the quotient in the ALU's <code>result</code> field and the remainder in the <code>supplemental_result</code> field when these are mathematically defined
+ */
+alu_result_t unsigned_divide(uint16_t dividend, uint16_t divisor) {
+    alu_result_t quotient;
+    return quotient;
+}
+
+/*
+ * SIGNED_MULTIPLY AND SIGNED_DIVIDE ARE FOR BONUS CREDIT.
+ * YOU ARE NOT REQUIRED AT ATTEMPT THEM.
+ */
+
+/**
+ * <p>Multiplies two 16-bit integers. The arguments are bit vectors that are interpreted as signed integers. The lower
+ * 16 bits of the full product are placed in the ALU's <code>result</code> field, and the upper 16 bits of the full
+ * product are placed in the ALU's <code>supplemental_result</code> field.</p>
+ *
+ * <p>This function sets the ALU's <code>divide_by_zero</code> flag to 0 but it does not alter the ALU's
+ * <code>unsigned_overflow</code> and <code>signed_overflow</code> flags.</p>
+ *
+ * @param multiplicand the number to be multiplied
+ * @param multiplier the number that the first is to be multiplied by
+ * @return the product in the ALU's <code>result</code> and <code>supplemental_result</code> fields
+ */
+alu_result_t signed_multiply(uint16_t multiplicand, uint16_t multiplier) {
+    alu_result_t product;
+    return product;
+}
+
+/**
+ * <p>Divides two 16-bit integers. The arguments are bit vectors that are interpreted as signed integers.</p>
+ *
+ * <p>The divisor <i>must</i> be zero or a power of two.</p>
+ *
+ * <p>If the divisor is non-zero, the quotient is placed in the ALU's <code>result</code> field, the modulus (or
+ * remainder) is placed in the ALU's <code>supplemental_result</code> field, and the <code>divide_by_zero</code> flag
+ * is set to 0.</p>
+ *
+ * <p>If the divisor is zero, the ALU's <code>divide_by_zero</code> flag is set to 1 and no guarantees are made about
+ * the contents of the <code>result</code> and <code>supplemental_result</code> fields.</p>
+ *
+ * <p>Regardless, the ALU's <code>unsigned_overflow</code> and <code>signed_overflow</code> flags are not altered.</p>
+ *
+ * @param dividend the number to be divided
+ * @param divisor the number that divides the first
+ * @return the ALU's <code>divide_by_zero</code> flag set appropriately, and the quotient in the ALU's <code>result</code> field and the remainder in the <code>supplemental_result</code> field when these are mathematically defined
+ */
+alu_result_t signed_divide(uint16_t dividend, uint16_t divisor) {
+    alu_result_t quotient;
+    return quotient;
 }
---
+++ IntegerLab/starter-code/basetwo.c
@@ -0,0 +1,46 @@
+/**************************************************************************//**
+ *
+ * @file basetwo.c
+ *
+ * @author Christopher A. Bohn
+ *
+ * @brief Functions that students must implement for IntegerLab to demonstrate
+ * understanding of base two exponentiation and logarithms.
+ *
+ ******************************************************************************/
+
+/*
+ * IntegerLab (c) 2018-22 Christopher A. Bohn
+ *
+ * Assignment and starter code licensed under the Apache License,
+ * Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
+ */
+
+#include "alu.h"
+
+/**
+ * Computes a power of two, specifically, the value of 2 raised to the power of <code>exponent</code>.
+ * foo == exponentiate(bar) \<--> bar == lg(foo). The exponent must be a non-negative value strictly less than 32.
+ * @param exponent the exponent to which 2 will be raised
+ * @return 2 raised to the power of <code>exponent</code>
+ */
+uint32_t exponentiate(int exponent) {
+    return 0;
+}
+
+/**
+ * Determines the base-two logarithm of an integer that is a power of two.
+ * foo == exponentiate(bar) \<--> bar == lg(foo). The argument must be a positive power of two.
+ * @param power_of_two the value whose logarithm will be determined
+ * @return base-2 logarithm of the argument
+ */
+int lg(uint32_t power_of_two) {
+    switch (power_of_two) {
+        case 0x1 :
+            return 0;
+        case 0x2 :
+            return 1;
+        default :
+            return -1;
+    }
+}
