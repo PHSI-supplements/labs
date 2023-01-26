027fbf0c1ed4057ebded5f32f95ebf9768612d07
Christopher Bohn <bohn@unl.edu>
2023-01-26 11:20:30 -0600

    Updated construction instructions based on student feedback

--- PokerLab/package.sh
+++ PokerLab/package.sh
@@ -7,7 +7,8 @@ cp ../assignment/pokerlab.pdf .
 #tar cvf pokerlab.tar * equivalent-java/*
 tar cvf pokerlab.tar *
 mv pokerlab.tar ..
-zip pokerlab.zip *
+#zip pokerlab.zip *
+zip pokerlab.zip * equivalent-java/*
 mv pokerlab.zip ..
 mv pokerlab.pdf ..
 cd ..
