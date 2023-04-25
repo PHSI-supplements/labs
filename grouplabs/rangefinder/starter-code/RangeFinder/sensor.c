c2fcb1352220fd05871270015b6b3f0ac7ef4033
Christopher Bohn <bohn@unl.edu>
2023-04-24 22:36:24 -0500

    prepared (most of) the RangeFinder GroupLab

---
+++ grouplabs/rangefinder/starter-code/RangeFinder/sensor.c
@@ -0,0 +1,32 @@
+/**************************************************************************//**
+ *
+ * @file sensor.c
+ *
+ * @author (STUDENTS -- TYPE YOUR NAMES HERE)
+ *
+ * @brief Code to manage the distance sensor.
+ *
+ ******************************************************************************/
+
+/*
+ * RangeFinder GroupLab assignment and starter code (c) 2023 Christopher A. Bohn
+ * RangeFinder solution (c) the above-named students
+ */
+
+#include <CowPi.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "sensor.h"
+#include "shared_variables.h"
+#include "supplement.h"
+
+
+void initialize_sensor(void) {
+
+}
+
+void manage_sensor(void) {
+
+}
